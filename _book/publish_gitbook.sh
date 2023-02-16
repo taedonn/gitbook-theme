515a82713f3cfb5447df9a4688e595a2d6826b80
xxDevBosa <grandsky95@hotmail.com>
2023-02-17 01:36:44 +0900

    Update docs

--- _book/publish_gitbook.sh
+++ _book/publish_gitbook.sh
@@ -1,19 +1,10 @@
 # gitbook 의존 파일을 설치하고 gitbook 빌드를 돌린다.
 gitbook build
 
-# github pages가 바라보는 gh-pages 브랜치를 만든다.
-git checkout gh-pages
-
-# 최신 gh-pages 브랜치 정보를 가져와 rebase를 진행한다.
-git pull origin gh-pages --rebase
-
 # gitbook build로 생긴 _book폴더 아래 모든 정보를 현재 위치로 가져온다.
 cp -R _book/* .
 
-# NOQA
-git add .
-
-# 커밋커밋!
+# 커밋
 git commit -a -m "Update docs"
 
 # gh-pages 브랜치에 PUSH!
