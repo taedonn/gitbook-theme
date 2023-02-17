159b49dd10d45af6772b8eda3bba9dff810507ba
LeeBosa <grandsky95@hotmail.com>
2023-02-17 14:17:58 +0900

    Update publish_gitbook.sh

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -1,19 +1,19 @@
 # main 브랜치로 접근
 git checkout main
 
-# gitbook 의존 파일을 설치하고 gitbook 빌드를 돌린다.
+# gitbook 빌드
 gitbook build
 
 # gh-pages로 접근
 git checkout gh-pages
 
-# gh-pages에 스테이징
+# gh-pages에 모든 변경사항 스테이징
 git add .
 
 # gh-pages에 커밋
 git commit -a -m "Update gitbook"
 
-# gh-pages 브랜치에 _book 파일 푸시
+# gh-pages에 _book 파일 푸시
 git subtree push --prefix _book origin gh-pages
 
 # 다시 master 브랜치로 돌아온다.
